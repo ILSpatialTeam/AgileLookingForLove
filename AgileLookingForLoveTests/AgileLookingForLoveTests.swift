be239e9950a277e6e7472cb9ca74923f9b54f7fa
Muhammad Benny Fathurrahman <bennyfathurr26@gmail.com>
2026-06-22 15:11:58 +0700

    Initial Commit

---
+++ AgileLookingForLoveTests/AgileLookingForLoveTests.swift
@@ -0,0 +1,19 @@
+//
+//  AgileLookingForLoveTests.swift
+//  AgileLookingForLoveTests
+//
+//  Created by Muhammad Benny Fathurrahman on 22/06/26.
+//
+
+import Testing
+@testable import AgileLookingForLove
+
+struct AgileLookingForLoveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
